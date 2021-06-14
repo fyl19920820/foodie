44934813e5fa7b0a65763ea36c04ff08566cee45
fengyunlong <fyl19920820@163.com>
2021-06-14 21:14:15 +0800

    设置跨域配置

---
+++ foodie-dev-api/src/main/java/com/imooc/config/CorsConfig.java
@@ -0,0 +1,32 @@
+package com.imooc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    public CorsConfig() {
+    }
+
+    @Bean
+    public CorsFilter corsFilter(){
+        //1.添加cros配置信息
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("http://localhost:8080");
+
+        //设置是否发送cookie信息
+        corsConfiguration.setAllowCredentials(true);
+
+        corsConfiguration.addAllowedMethod("*");
+
+        corsConfiguration.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        corsConfigurationSource.registerCorsConfiguration("/**",corsConfiguration);
+
+        return new CorsFilter(corsConfigurationSource);
+    }
+}
