81d54a3f657c0e8a98cd3756a501805a8051c3a5
fengyunlong <fyl19920820@163.com>
2021-06-30 14:54:32 +0800

    订单查询 商家发货 确认收货和删除订单 查询待评价商品列表 评价商品 历史评价列表展示 订单状态及订单动向展示

--- foodie-dev-api/src/main/java/com/imooc/controller/BaseController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/BaseController.java
@@ -1,5 +1,9 @@
 package com.imooc.controller;
 
+import com.imooc.pojo.Orders;
+import com.imooc.service.center.MyOrdersService;
+import com.imooc.utils.IMOOCJSONResult;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -16,4 +20,19 @@ public class BaseController {
     // 微信支付成功 -> 支付中心 -> 天天吃货平台
     //                       |-> 回调通知的url
     String payReturnUrl = "http://xktmds.natappfree.cc/orders/notifyMerchantOrderPaid";
+
+    @Autowired
+    public MyOrdersService myOrdersService;
+
+    /**
+     * 用于验证用户和订单是否有关联关系，避免非法用户调用
+     * @return
+     */
+    public IMOOCJSONResult checkUserOrder(String userId, String orderId) {
+        Orders order = myOrdersService.queryMyOrder(userId, orderId);
+        if (order == null) {
+            return IMOOCJSONResult.errorMsg("订单不存在！");
+        }
+        return IMOOCJSONResult.ok(order);
+    }
 }
