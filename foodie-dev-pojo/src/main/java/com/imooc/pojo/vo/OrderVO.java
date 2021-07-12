6d2c9878a748a81210172204f3a213808ac499ba
fengyunlong <fyl19920820@163.com>
2021-07-12 21:47:34 +0800

    增加redis 轮播图 购物车添加商品和删除商品 下单商品数量 购物车同步 用户退出等功能进行相应改造

--- foodie-dev-pojo/src/main/java/com/imooc/pojo/vo/OrderVO.java
+++ foodie-dev-pojo/src/main/java/com/imooc/pojo/vo/OrderVO.java
@@ -1,9 +1,23 @@
 package com.imooc.pojo.vo;
 
+import com.imooc.pojo.bo.ShopcartBO;
+
+import java.util.List;
+
 public class OrderVO {
 
     private String orderId;
     private MerchantOrdersVO merchantOrdersVO;
+
+    public List<ShopcartBO> getToBeRemovedShopcatdList() {
+        return toBeRemovedShopcatdList;
+    }
+
+    public void setToBeRemovedShopcatdList(List<ShopcartBO> toBeRemovedShopcatdList) {
+        this.toBeRemovedShopcatdList = toBeRemovedShopcatdList;
+    }
+
+    private List<ShopcartBO> toBeRemovedShopcatdList;
 
     public String getOrderId() {
         return orderId;
