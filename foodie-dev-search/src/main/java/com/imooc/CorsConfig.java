77ab7a5a8129abd7440ea8f32c26edc7c3ba0c08
fengyunlong <fyl19920820@163.com>
2021-07-31 18:01:18 +0800

    elasticsearch 集成

---
+++ foodie-dev-search/src/main/java/com/imooc/CorsConfig.java
@@ -0,0 +1,36 @@
+package com.imooc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    public CorsConfig() {
+    }
+
+    @Bean
+    public CorsFilter corsFilter(){
+        //1.添加cros配置信息
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("http://localhost:8080");
+        corsConfiguration.addAllowedOrigin("http://49.233.2.43:8080");
+        corsConfiguration.addAllowedOrigin("http://49.233.2.43:8000");
+        corsConfiguration.addAllowedOrigin("http://49.233.2.43:8001");
+        corsConfiguration.addAllowedOrigin("*");
+
+        //设置是否发送cookie信息
+        corsConfiguration.setAllowCredentials(true);
+
+        corsConfiguration.addAllowedMethod("*");
+
+        corsConfiguration.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        corsConfigurationSource.registerCorsConfiguration("/**",corsConfiguration);
+
+        return new CorsFilter(corsConfigurationSource);
+    }
+}
